5289ce306091c67afe7674fc3fc335ddbf8cf698
godeh <gabbyjessie44@gmail.com>
2026-09-10 00:22:59 +0100

    schema sql

--- database/schema.sql
+++ database/schema.sql
@@ -14,13 +14,11 @@ CREATE TABLE associations (
     association_img_url VARCHAR(255) DEFAULT NULL,
     public_association_unique_code VARCHAR(50) NOT NULL UNIQUE,
     association_slug VARCHAR(100) NOT NULL UNIQUE,
-    created_by INT NOT NULL
+    created_by INT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
 
-    FOREIGN KEY (created_by) REFERENCES users(user_id) ON DELETE CASCADE
+    FOREIGN KEY (created_by) REFERENCES users(user_id) ON DELETE RESTRICT
 );
 
-CREATE TABLE meetingrecord (
-    meeting_id INT PRIMARY KEY AUTO_INCREMENT,
 
-)
+
